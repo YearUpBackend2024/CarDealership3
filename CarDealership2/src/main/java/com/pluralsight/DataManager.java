70b9576f762482aa063e98c87e3cf0c89670e292
agabin <agabin@appdev.yearup.org>
2024-12-05 15:12:12 -0500

    Created Database

---
+++ CarDealership2/src/main/java/com/pluralsight/DataManager.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+import java.util.List;
+
+public class DataManager {
+
+    public List<Vehicle> getVehiclesByPrice(int dealershipId, double min, double max){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByVin(int VIN){
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles(int dealershipId){
+        return null;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        //insert vehicle into database
+    }
+
+
+
+
+}
