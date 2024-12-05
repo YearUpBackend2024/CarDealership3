2b6c1a1b065c0378e2f76fbeeb311453d44fc2e0
agabin <agabin@appdev.yearup.org>
2024-12-05 16:11:15 -0500

    added methods to interact with database

--- CarDealership2/src/main/java/com/pluralsight/DataManager.java
+++ CarDealership2/src/main/java/com/pluralsight/DataManager.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.security.PublicKey;
 import java.util.List;
 
 public class DataManager {
@@ -8,7 +9,27 @@ public class DataManager {
         return null;
     }
 
+    public List<Vehicle> getVehiclesByMileage(double min, double max){
+        return null;
+    }
+
     public List<Vehicle> getVehiclesByVin(int VIN){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String type){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByYear(double min, double max){
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByColor(String color){
         return null;
     }
 
@@ -20,7 +41,9 @@ public class DataManager {
         //insert vehicle into database
     }
 
-
+    public void removeVehicle(Vehicle vehicle){
+        //remove vehicle from database
+    }
 
 
 }
